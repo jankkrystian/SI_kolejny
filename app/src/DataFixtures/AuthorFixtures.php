ea5ea1ae951ea88a212ce2e58b37ed4460128df0
jankkrystian <you@example.com>
2023-07-27 18:28:57 +0200

    stan poprzedni, będę upiękniała trochę całość

--- app/src/DataFixtures/AuthorFixtures.php
+++ app/src/DataFixtures/AuthorFixtures.php
@@ -6,46 +6,25 @@
 namespace App\DataFixtures;
 
 use App\Entity\Author;
-use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
-use Faker\Factory;
-use Faker\Generator;
 
 /**
  * Class AuthorFixtures.
  */
-class AuthorFixtures extends Fixture
+class AuthorFixtures extends AbstractBaseFixtures
 {
     /**
-     * Faker.
-     *
-     * @var Generator
+     * Load data.
      */
-    protected Generator $faker;
-
-    /**
-     * Persistence object manager.
-     *
-     * @var ObjectManager
-     */
-    protected ObjectManager $manager;
-
-    /**
-     * Load.
-     *
-     * @param ObjectManager $manager Persistence object manager
-     */
-    public function load(ObjectManager $manager): void
+    public function loadData(): void
     {
-        $this->faker = Factory::create();
-
         for ($i = 0; $i < 10; ++$i) {
             $author = new Author();
             $author->setName($this->faker->sentence);
             $author->setSurname($this->faker->sentence);
-            $manager->persist($author);
+            $this->manager->persist($author);
         }
 
-        $manager->flush();
+        $this->manager->flush();
     }
 }
